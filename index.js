cafa2f2bc1c2a37ea7142f703c250be7fe2195c3
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2022-08-23 15:23:39 +0300

    complited assinement

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400){
+      return 'This one is on me!';
+    } else if (someValue <= 2000){
+      return 'That will be twenty bucks.';
+    } else if (someValue <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else {
+      return 'No can do.';
+    };
+  }
+  
+  function ternaryCheckCity(city){
+
+//   // Write your code here!
+      return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+//   // Write your code here!
+switch(tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+  default:
+    return 'Bye.';
+    break;
+}
 }
